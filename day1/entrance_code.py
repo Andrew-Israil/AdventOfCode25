5e08e54a0aefc5340a2fda4cc7235a9ed42bfb9d
Andrew-Israil <andrewkerollos@gmail.com>
2026-01-09 16:53:54 +0100

    day1 solution

--- day1/entrance_code.py
+++ day1/entrance_code.py
@@ -2,5 +2,39 @@ import sys
 
 
 with open(sys.argv[1], "r") as f:
-    lines = list(map(str.strip, f.readlines()))
+    rotations = list(map(str.strip, f.readlines()))
 
+dial = 50
+last_step = 99
+total_steps = 100
+zeros = 0
+
+for rotation in rotations:
+    direction = rotation[0]
+    clicks_num = int(rotation[1:])
+
+    print(f"dial = {dial}")
+    print(f"operation = {rotation}")
+
+    remaining_right_steps = last_step - dial
+
+    if(clicks_num > total_steps):
+        clicks_num = clicks_num % total_steps
+
+    if(direction == "R"):
+        if(clicks_num > remaining_right_steps):
+            dial = clicks_num - remaining_right_steps - 1
+        else:
+            dial = dial + clicks_num
+
+    else:
+        if(clicks_num > dial):
+            dial = last_step - (clicks_num - dial - 1)
+        else:
+            dial = dial - clicks_num
+        
+    print(f"dial = {dial}")
+    if dial == 0:
+        zeros += 1
+
+print(f"zeros = {zeros}")
